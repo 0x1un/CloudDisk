b57cab1e8a2f170c88ef6e871598f7b9635e517a
0x1un <aumujun@gmail.com>
2019-09-10 16:39:15 +0800

    add batchQuery router

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ func main() {
 	log.Println("Starting server...")
 	http.HandleFunc("/file/upload", handler.UploadHandler)
 	http.HandleFunc("/file/upload/success", handler.UploadSuccessHandler)
-	http.HandleFunc("/file/meta", handler.GetFileMetaByMD5)
+	http.HandleFunc("/file/meta", handler.GetFileMetaByMD5Handler)
+	http.HandleFunc("/file/batchQuery", handler.GetRecentFileMetasHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
