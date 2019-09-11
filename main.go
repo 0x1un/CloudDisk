a136b5f56ee8c84f66f900d068bc4b7e0078e123
0x1un <aumujun@gmail.com>
2019-09-11 13:18:07 +0800

    add Download Router

--- main.go
+++ main.go
@@ -7,11 +7,13 @@ import (
 	"github.com/0x1un/CloudDisk/handler"
 )
 
+// main: beging
 func main() {
 	log.Println("Starting server...")
 	http.HandleFunc("/file/upload", handler.UploadHandler)
 	http.HandleFunc("/file/upload/success", handler.UploadSuccessHandler)
 	http.HandleFunc("/file/meta", handler.GetFileMetaByMD5Handler)
 	http.HandleFunc("/file/batchQuery", handler.GetRecentFileMetasHandler)
+	http.HandleFunc("/file/download", handler.DownloadHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
