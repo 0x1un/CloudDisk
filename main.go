b92f8847bd619d3f9be59468e1dc7d68c9419617
0x1un <aumujun@gmail.com>
2019-09-11 17:04:44 +0800

    add delete router

--- main.go
+++ main.go
@@ -16,5 +16,6 @@ func main() {
 	http.HandleFunc("/file/batchQuery", handler.GetRecentFileMetasHandler)
 	http.HandleFunc("/file/download", handler.DownloadHandler)
 	http.HandleFunc("/file/updateName", handler.FileUpdateMetaHandler)
+	http.HandleFunc("/file/delete", handler.DeleteFileHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
