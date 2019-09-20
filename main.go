72f1da23caf9734ef57c8d4ed795ccc5a6baa5d0
0x1un <aumujun@gmail.com>
2019-09-20 17:40:37 +0800

    add registration handler

--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/0x1un/CloudDisk/handler"
+	"github.com/0x1un/CloudDisk/util"
 )
 
 // main: beging
 func main() {
+	hash, err := util.EncodePWDToBcrpty("goodluck@123")
+	if err != nil {
+		return
+	}
+	fmt.Println(string(hash))
 	log.Println("Starting server...")
+	log.Printf("Please open browser and paste the url: %s\n", "http://localhost"+util.Conf.Port)
 	http.HandleFunc("/file/upload", handler.UploadHandler)
 	http.HandleFunc("/file/upload/success", handler.UploadSuccessHandler)
 	http.HandleFunc("/file/meta", handler.GetFileMetaByMD5Handler)
@@ -17,5 +25,6 @@ func main() {
 	http.HandleFunc("/file/download", handler.DownloadHandler)
 	http.HandleFunc("/file/updateName", handler.FileUpdateMetaHandler)
 	http.HandleFunc("/file/delete", handler.DeleteFileHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/user/signup", handler.UserSignupHandler)
+	log.Fatal(http.ListenAndServe(util.Conf.Port, nil))
 }
