b854bea43a24833a4f494e3778259a344d8f26b0
0x1un <aumujun@gmail.com>
2019-09-20 09:31:55 +0800

    replace Create with FirstOrCreate

--- db/user.go
+++ db/user.go
@@ -18,9 +18,10 @@ type Users struct {
 	Status        int
 }
 
+// UserSignupInsertToDB: user signup and insert user profile into postgresql
 func UserSignupInsertToDB(user *Users) bool {
 	insert := pg.DBConnect().Begin()
-	if err := insert.Table("users").Create(user).Error; err != nil {
+	if err := insert.Table("users").FirstOrCreate(user).Error; err != nil {
 		insert.Rollback()
 		fmt.Printf("Failed to insert user: %s\n", err.Error())
 		return false
