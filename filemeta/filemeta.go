8a760a82469359b2efea36e212d87c8fa489c1ff
0x1un <aumujun@gmail.com>
2019-09-21 10:41:57 +0800

    remove FileMeta struct

--- filemeta/filemeta.go
+++ filemeta/filemeta.go
@@ -5,38 +5,29 @@ import (
 	"sort"
 
 	db "github.com/0x1un/CloudDisk/db"
-	pg "github.com/0x1un/CloudDisk/db/pg"
 )
 
-type FileMeta struct {
-	FileMD5  string
-	FileName string
-	FileSize int64
-	Location string
-	UploadAt string // format time: 2006-09-01 15:04:06
-	Status   int
-}
-
 // fileMetas: store file meta info
-var fileMetas map[string]FileMeta
+var fileMetas map[string]db.TableFileMeta
 
 // init: to initalize fileMetas
 func init() {
-	fileMetas = make(map[string]FileMeta)
+	fileMetas = make(map[string]db.TableFileMeta)
 }
 
 // UpdateFileMeta: update or create file meta, key: file.md5, value: FileMeta struct
-func UpdateFileMeta(filemeta FileMeta) {
+func UpdateFileMeta(filemeta db.TableFileMeta) {
 	fileMetas[filemeta.FileMD5] = filemeta
 }
 
 // UpdateFileMetaDB: store file meta into postgres
-func UpdateFileMetaDB(filemeta *FileMeta) bool {
-	return onFileUploadFinished(filemeta)
+func UpdateFileMetaDB(filemeta *db.TableFileMeta) bool {
+	// return onFileUploadFinished(filemeta)
+	return db.OnFileUploadFinished(filemeta)
 }
 
 // GetFileMeta: return a filemeta by file md5 value
-func GetFileMeta(filemd5 string) FileMeta {
+func GetFileMeta(filemd5 string) db.TableFileMeta {
 	return fileMetas[filemd5]
 }
 
@@ -60,18 +51,4 @@ func GetRecentFileMetas(limit int) []db.TableFileMeta {
 // DeleteFileMeta: delete file meta from fileMetas map
 func DeleteFileMeta(filemd5 string) {
 	delete(fileMetas, filemd5)
-}
-
-// OnFileUploadFinished: store file meta into postgres
-func onFileUploadFinished(fmeta *FileMeta) bool {
-	insert := pg.DBConnect().Begin()
-	if err := insert.Table("filemetas").Create(fmeta).Error; err != nil {
-		insert.Rollback()
-		fmt.Printf("Failed insert to tables: %s", err.Error())
-		return false
-	}
-	// defer insert.Close()
-	insert.Commit()
-
-	return true
 }
