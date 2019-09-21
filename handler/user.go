bd7706f3532b801623bff375f5374b013886c2e7
0x1un <aumujun@gmail.com>
2019-09-21 17:54:07 +0800

    added some login signup handler's

--- handler/user.go
+++ handler/user.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -20,7 +22,8 @@ func UserSignupHandler(w http.ResponseWriter, r *http.Request) {
 		} else {
 			w.Write(data)
 		}
-	} else if r.Method == http.MethodPost {
+	}
+	if r.Method == http.MethodPost {
 		username := r.FormValue("username")
 		password := r.FormValue("password")
 		if len(username) < 3 || len(password) < 5 {
@@ -48,4 +51,70 @@ func UserSignupHandler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 	}
+}
+
+func UserLoginHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+
+		data, err := ioutil.ReadFile("./static/view/signin.html")
+		if err != nil {
+			log.Fatal("Failed to open signin.html")
+			w.WriteHeader(http.StatusNotFound)
+			return
+		} else {
+			w.Write(data)
+		}
+	}
+	if r.Method == http.MethodPost {
+
+		r.ParseForm()
+		username := r.Form.Get("username")
+		fmt.Println(username)
+		password := r.Form.Get("password")
+		fmt.Println(password)
+		isOk := db.UserLoginMatcher(username, password)
+		if isOk {
+			token := GenerateToken(username)
+			updateOk := db.UpdateUserToken(&db.UserTokens{
+				UserName:  username,
+				UserToken: token,
+			})
+			if !updateOk {
+				w.Write([]byte("FAILED"))
+				return
+			}
+		}
+	}
+	w.Write([]byte("http://" + r.Host + "/static/view/home.html"))
+}
+
+// UserProfileHandler: get user info
+func UserProfileHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	username := r.Form.Get("username")
+	user, err := db.GetUserInfo(username)
+	fmt.Println(user)
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		return
+	}
+	data, _ := json.Marshal(user)
+	w.Write(data)
+	return
+}
+
+// GenerateToken: generate token with username
+func GenerateToken(username string) string {
+	// md5(username + timestamp + token_salt) + timestamp[:8] + reverse(timestamp)
+	// total length: 48
+	timestamp := fmt.Sprintf("%x", time.Now().Unix())
+	reverseTimeStamp := func(timestamp string) string {
+		runes := []rune(timestamp)
+		for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+			runes[i], runes[j] = runes[j], runes[i]
+		}
+		return string(runes)
+	}(timestamp)
+	data := util.ComputeMD5FromString(fmt.Sprintf("%s%s%s", username, timestamp, "_biubiubiuxxxoo"))
+	return fmt.Sprintf("%s%s%s", data, timestamp[:8], reverseTimeStamp)
 }
