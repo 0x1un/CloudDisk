bc652d408f5d5d16f81cb6f77455465027df8534
0x1un <aumujun@gmail.com>
2019-09-21 17:55:15 +0800

    generate md5 from string and compare password

--- util/util.go
+++ util/util.go
@@ -24,16 +24,31 @@ func ComputeFileMD5(filename string) string {
 	return hex.EncodeToString(sha.Sum(nil))
 }
 
+func ComputeMD5FromString(str string) string {
+	sha := md5.New()
+	_, err := sha.Write([]byte(str))
+	if err != nil {
+		return ""
+	}
+	return hex.EncodeToString(sha.Sum(nil))
+}
+
 var (
 	pwdSalt = "o0Oao&#$%^10xiIill1"
 )
 
 func EncodePWDToBcrpty(pwd string) ([]byte, error) {
-	pwd = pwdSalt + pwd
 	hash, err := bcrypt.GenerateFromPassword([]byte(pwd), bcrypt.DefaultCost)
 	if err != nil {
-		log.Fatalf("Failed bcrypt: %s\n", err.Error())
 		return nil, err
 	}
 	return hash, nil
+}
+
+func ComparePWD(rawPwd, encodePWD string) bool {
+	if err := bcrypt.CompareHashAndPassword([]byte(rawPwd), []byte(encodePWD)); err != nil {
+		log.Println(err)
+		return false
+	}
+	return true
 }
