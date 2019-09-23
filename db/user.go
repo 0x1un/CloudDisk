fc647b0c225f2325f35dc73fcfa09d1330db6a78
aumujun <aumujun@gmail.com>
2019-09-23 22:15:06 +0800

    user info

--- db/user.go
+++ db/user.go
@@ -62,7 +62,7 @@ func UserLoginMatcher(username string, userpwd string) bool {
 func GetUserInfo(username string) (*Users, error) {
 	tmpUser := &Users{}
 	handler := pg.DBConnect().Table("users")
-	isNotFound := handler.Select("*").Where("user_name = ? and status = 1", username).Find(tmpUser).RecordNotFound()
+	isNotFound := handler.Select("user_name,signup_at").Where("user_name = ? and status = 1", username).Find(tmpUser.UserName, tmpUser.SignupAt).RecordNotFound()
 	if isNotFound {
 		return nil, errors.New("username does not exsists!")
 	}
