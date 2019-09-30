adfc6f5c4d94507c4029320b3e5f866b8d4f658e
0x1un <aumujun@gmail.com>
2019-09-30 16:26:55 +0800

    fix

--- filemeta/sort.go
+++ filemeta/sort.go
@@ -1,14 +1,12 @@
 package filemeta
 
 import (
-	"time"
-
 	db "github.com/0x1un/CloudDisk/db"
 )
 
 type ByUploadAtTime []db.TableFileMeta
 
-const baseFortmatTime = "2006-01-02 15:04:05"
+// const baseFortmatTime = "2006-01-02 15:04:05"
 
 func (a ByUploadAtTime) Len() int {
 	return len(a)
@@ -19,7 +17,10 @@ func (a ByUploadAtTime) Swap(i, j int) {
 }
 
 func (a ByUploadAtTime) Less(i, j int) bool {
-	iTime, _ := time.Parse(baseFortmatTime, a[i].UploadAt)
-	jTime, _ := time.Parse(baseFortmatTime, a[j].UploadAt)
-	return iTime.UnixNano() > jTime.UnixNano()
+	// iTime, _ := time.Parse(baseFortmatTime, a[i].UploadAt)
+	iTime := a[i].UploadAt.UnixNano()
+	// jTime, _ := time.Parse(baseFortmatTime, a[j].UploadAt)
+	jTime := a[j].UploadAt.UnixNano()
+	// return iTime.UnixNano() > jTime.UnixNano()
+	return iTime > jTime
 }
