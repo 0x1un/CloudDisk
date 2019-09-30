75731081eed893392ff10122be68c636b4d74a4a
0x1un <aumujun@gmail.com>
2019-09-30 16:26:20 +0800

    user files store into db

--- db/userfile.go
+++ db/userfile.go
@@ -1,8 +1,29 @@
 package db
 
+import (
+	"time"
+
+	pg "github.com/0x1un/CloudDisk/db/pg"
+)
+
+// UserFile: 用户文件信息表
 type UserFile struct {
+	UserName   string
+	FileMd5    string
+	FileSize   int64
+	FileName   string
+	UploadAt   time.Time `gorm:"default:current_time"`
+	LastUpdate time.Time `gorm:"default:current_time"`
+	Status     int
 }
 
+// UploadUserFileDB: 用户上传文件的信息
 func UploadUserFileDB(userfile *UserFile) bool {
-
+	handler := pg.DBConnect().Begin()
+	if err := handler.Table("user_files").Create(userfile).Error; err != nil {
+		handler.Rollback()
+		return false
+	}
+	handler.Commit()
+	return true
 }
